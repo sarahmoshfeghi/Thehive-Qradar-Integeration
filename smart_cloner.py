ffbf28218af73b3798aae17c29dbdb36bb65baeb
sarahmoshfeghi <134586796+sarahmoshfeghi@users.noreply.github.com>
2025-03-10 11:37:38 +0330

    Update smart_cloner.py

--- smart_cloner.py
+++ smart_cloner.py
@@ -1,45 +1,43 @@
-#!/usr/bin/env python
-
-import argparse
+import os
 import logging
 import logging.config
-import json
-import os
-import platform
-
 from objects.common import getConf, setConf
 from objects.offense2alert import allOffense2Alert
 
-## logger configuration
-currentPath = os.path.dirname(os.path.abspath(__file__))
-loggerConfPath = currentPath + '/conf/log.conf'
-logging.config.fileConfig(loggerConfPath)
+def qradar2thehive():
+    ## logger configuration
+    currentPath = os.path.dirname(os.path.abspath(__file__))
+    loggerConfPath = currentPath + '/conf/log.conf'
+    logging.config.fileConfig(loggerConfPath)
 
-logger = logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)
 
-logger.info('%s.SMART_CLONNER', __name__)
+    logger.info('%s.SMART_CLONNER', __name__)
 
-cfg = getConf()
+    cfg = getConf()
 
-scStatus = cfg['smartclonner']['status']
-logger.info("smartclonner status: " + scStatus)
+    scStatus = cfg['smartclonner']['status']
+    logger.info("smartclonner status: " + scStatus)
 
-if int(scStatus) == 0:
+    if int(scStatus) == 0:
         try:
-                cfg['smartclonner']['status'] = str(1)
-                setConf(cfg)
-                logger.info("launching clonning offenses as alert")
-                report = allOffense2Alert()
-                for reportOffense in report['offenses']:
-                        logger.info("Is offense " + str(reportOffense['qradar_offense_id']) + \
-                                        " clonned to alert " + str(reportOffense['raised_alert_id']) + \
-                                        " : " + str(reportOffense['success']))
+            cfg['smartclonner']['status'] = str(1)
+            setConf(cfg)
+            logger.info("launching clonning offenses as alert")
+            report = allOffense2Alert()
+            for reportOffense in report['offenses']:
+                logger.info("Is offense " + str(reportOffense['qradar_offense_id']) +
+                            " clonned to alert " + str(reportOffense['raised_alert_id']) +
+                            " : " + str(reportOffense['success']))
         except Exception as ex:
-                logger.error("Tool exception: " + str(ex))
-        
+            logger.error("Tool exception: " + str(ex))
+
         # set status to "completed"
         cfg = getConf()
         cfg['smartclonner']['status'] = str(0)
         setConf(cfg)
-else:
+    else:
         logger.info("another instance of smartclonner has been launched")
+
+if __name__ == "__main__":
+    qradar2thehive()
